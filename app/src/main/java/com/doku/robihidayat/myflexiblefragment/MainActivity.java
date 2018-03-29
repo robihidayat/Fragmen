d13880b46b6a8fb45d122d6c18af9c33febaabb7
robihidayat <robi.hidayat@mail.ugm.ac.id>
2018-03-29 08:58:16 +0700

    first commit

---
+++ app/src/main/java/com/doku/robihidayat/myflexiblefragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.doku.robihidayat.myflexiblefragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        android.support.v4.app.FragmentManager mFragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+
+        HomeFragment mHomeFragment = new HomeFragment();
+        mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+        Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment.class.getSimpleName());
+        mFragmentTransaction.commit();
+
+    }
+}
