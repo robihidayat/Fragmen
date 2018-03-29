d438b1aff6fa2b682b9cef55c317eee0908197eb
robihidayat <robi.hidayat@mail.ugm.ac.id>
2018-03-29 09:16:37 +0700

    add details categori

--- app/src/main/java/com/doku/robihidayat/myflexiblefragment/CategoryFragment.java
+++ app/src/main/java/com/doku/robihidayat/myflexiblefragment/CategoryFragment.java
@@ -3,6 +3,8 @@ package com.doku.robihidayat.myflexiblefragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,8 +35,19 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-    if (v.getId() == R.id.btn_detail_category){
-
-    }
+        if (v.getId() == R.id.btn_detail_category){
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Kategori ini akan berisi produk-produk lifestyle";
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.frame_container, mDetailCategoryFragment,
+                    DetailCategoryFragment.class.getSimpleName());
+            mFragmentTransaction.addToBackStack(null);
+            mFragmentTransaction.commit();
+        }
     }
 }
